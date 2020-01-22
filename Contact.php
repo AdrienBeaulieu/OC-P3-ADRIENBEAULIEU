880ae7fed2dc594e9cfed0b3226b7e4644e0a7d8
AdrienBeaulieu <adrienbeaulieu@free.fr>
2020-01-22 12:41:27 +0100

    Ajout formulaire de contact sur Contact.php

--- Contact.php
+++ Contact.php
@@ -15,6 +15,55 @@
 			<div class="nom_prenom"> Nom & Prénom </div>
 			<hr class="barre_header" clolor="grey">
 		</header>
+<?php
+if(isset($_POST['mailform']))
+{
+	if(!empty($_POST['nom']) AND !empty($_POST['mail']) AND !empty($_POST['message']))
+	{
+		$header="MIME-Version: 1.0\r\n";
+		$header.='From:"GBAF.fr"<support@GBAF.com>'."\n";
+		$header.='Content-Type:text/html; charset="uft-8"'."\n";
+		$header.='Content-Transfer-Encoding: 8bit';
+
+		$message='
+		<html>
+			<body>
+				<div align="center">
+					<img src="logogbaf.png"/>
+					<br />
+					<u>Nom de l\'expéditeur :</u>'.$_POST['nom'].'<br />
+					<u>Mail de l\'expéditeur :</u>'.$_POST['mail'].'<br />
+					<br />
+					'.nl2br($_POST['message']).'
+					<br />
+					<img src="logogbaf.png"/>
+				</div>
+			</body>
+		</html>
+		';
+
+		mail("Sp33dwar14310@gmail.com", "CONTACT - GBAF", $message, $header);
+		$msg="Votre message a bien été envoyé !";
+	}
+	else
+	{
+		$msg="Tous les champs doivent être complétés !";
+	}
+}
+?>
+<h2>Formulaire de contact !</h2>
+		<form method="POST" action="">
+			<input type="text" name="nom" placeholder="Votre nom" value="<?php if(isset($_POST['nom'])) { echo $_POST['nom']; } ?>" /><br /><br />
+			<input type="email" name="mail" placeholder="Votre email" value="<?php if(isset($_POST['mail'])) { echo $_POST['mail']; } ?>" /><br /><br />
+			<textarea name="message" placeholder="Votre message"><?php if(isset($_POST['message'])) { echo $_POST['message']; } ?></textarea><br /><br />
+			<input type="submit" value="Envoyer !" name="mailform"/>
+		</form>
+		<?php
+		if(isset($msg))
+		{
+			echo $msg;
+		}
+		?>
 
 
 <!-- Footer -->
