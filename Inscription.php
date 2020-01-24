8931fe94d85952a28b6c30c595a834875e1f2879
AdrienBeaulieu <adrienbeaulieu@free.fr>
2020-01-24 12:19:52 +0100

    Création page inscription

---
+++ Inscription.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>S'inscrire</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form method="POST" action="">
+	<div align="center">
+				<h2> Inscription </h2>
+		<br><br><br>
+		<table>
+			<tr align="right">
+				<td>
+				<label for="Nom">Nom :</label>
+				</td>
+				<td>
+				<input type="text" placeholder="Nom" id="Nom" name="Nom">
+				</td>
+			</tr>
+			<tr align="right">
+				<td>
+				<label for="Prénom">Prénom :</label>
+				</td>
+				<td>
+				<input type="text" placeholder="Prénom" id="Prénom" name="Prénom">
+				</td>
+			</tr>
+			<tr align="right">
+				<td>
+				<label for="mail">E-mail :</label>
+				</td>
+				<td>
+				<input type="text" placeholder="Mail" id="Mail" name="Mail">
+				</td>
+			</tr>
+			<tr align="right">
+				<td>
+				<label for="motdepasse">Mot de passe :</label>
+				</td>
+				<td>
+				<input type="password" placeholder="mot de passe" id="motdepasse" name="motdepasse">
+				</td>
+			</tr align="right">
+		</table>
+	</div>
+	</body>
+</html>
