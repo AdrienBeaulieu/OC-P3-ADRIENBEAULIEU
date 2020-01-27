2a6679980cd11ea34a295c177ea26f9a7a607a48
AdrienBeaulieu <adrienbeaulieu@free.fr>
2020-01-27 14:49:08 +0100

    FIX CONNEXION --> Connexion par username + password

--- connexion.php
+++ connexion.php
@@ -4,21 +4,21 @@ session_start();
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
 if(isset($_POST['formconnexion'])) {
-   $mailconnect = htmlspecialchars($_POST['mailconnect']);
+   $usernameconnect = htmlspecialchars($_POST['usernameconnect']);
    $mdpconnect = sha1($_POST['mdpconnect']);
-   if(!empty($mailconnect) AND !empty($mdpconnect)) {
-      $requser = $bdd->prepare("SELECT * FROM membres WHERE mail = ? AND motdepasse = ?");
-      $requser->execute(array($mailconnect, $mdpconnect));
+   if(!empty($usernameconnect) AND !empty($mdpconnect)) {
+      $requser = $bdd->prepare("SELECT * FROM membres WHERE username = ? AND motdepasse = ?");
+      $requser->execute(array($usernameconnect, $mdpconnect));
       $userexist = $requser->rowCount();
       if($userexist == 1) {
          $userinfo = $requser->fetch();
          $_SESSION['id'] = $userinfo['id'];
          $_SESSION['nom'] = $userinfo['nom'];
          $_SESSION['prenom'] = $userinfo['prenom'];
-         $_SESSION['mail'] = $userinfo['mail'];
+         $_SESSION['username'] = $userinfo['username'];
          header("Location: Pageprincipal.php?id=".$_SESSION['id']);
       } else {
-         $erreur = "Mauvais mail ou mot de passe !";
+         $erreur = "Mauvais pseudonyme ou mot de passe !";
       }
    } else {
       $erreur = "Tous les champs doivent être complétés !";
@@ -36,7 +36,7 @@ if(isset($_POST['formconnexion'])) {
          <h2>Connexion</h2>
          <br /><br />
          <form method="POST" action="">
-            <input type="email" name="mailconnect" placeholder="Mail" />
+            <input type="text" name="usernameconnect" placeholder="pseudonyme" />
             <input type="password" name="mdpconnect" placeholder="Mot de passe" />
             <br /><br />
             <input type="submit" name="formconnexion" value="Se connecter !" />
